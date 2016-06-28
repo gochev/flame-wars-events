2ac6691fce05e76bfb34d77db963ceda61021764
Vladimir Tsanev <tsachev@gmail.com>
2016-06-28 19:58:20 +0300

    fix model

--- src/main/java/com/example/repository/RegistrationRepository.java
+++ src/main/java/com/example/repository/RegistrationRepository.java
@@ -2,10 +2,12 @@ package com.example.repository;
 
 import com.example.model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author tsachev
  */
+@RepositoryRestResource
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
 }
